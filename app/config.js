c1c0e9134327b201327030e87cbd1cfb10ef2646
Scott Lee <seungyol72@gmail.com>
2016-11-25 19:56:23 +1000

    Make bundle file for the dependent files

--- app/config.js
+++ app/config.js
@@ -7,9 +7,37 @@ System.config({
     "material-start/*": "src/*.js",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*",
-    'jasmine' : '../node_modules/jasmine-core/lib/jasmine-core/jasmine.js',
-    'jasmine-html' : '../node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js',
-    'jasmine-boot' : '../node_modules/jasmine-core/lib/jasmine-core/boot.js'      
+    "jasmine": "../node_modules/jasmine-core/lib/jasmine-core/jasmine.js",
+    "jasmine-html": "../node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js",
+    "jasmine-boot": "../node_modules/jasmine-core/lib/jasmine-core/boot.js"
+  },
+  bundles: {
+    "build.js": [
+      "material-start/boot/boot",
+      "material-start/AppController",
+      "npm:jquery@3.1.1.js",
+      "npm:jquery@3.1.1/dist/jquery.js",
+      "material-start/app",
+      "material-start/library/Library",
+      "material-start/library/components/details/LibraryDetails",
+      "material-start/library/components/details/LibraryDialog",
+      "material-start/library/components/list/LibraryList",
+      "github:angular/bower-angular-messages@1.5.8.js",
+      "github:angular/bower-angular-messages@1.5.8/angular-messages.js",
+      "github:angular/bower-angular@1.5.8.js",
+      "github:angular/bower-angular@1.5.8/angular.js",
+      "github:angular/bower-angular-route@1.5.8.js",
+      "github:angular/bower-angular-route@1.5.8/angular-route.js",
+      "github:angular/bower-angular-resource@1.5.8.js",
+      "github:angular/bower-angular-resource@1.5.8/angular-resource.js",
+      "github:angular/bower-material@master.js",
+      "github:angular/bower-material@master/index.js",
+      "github:angular/bower-material@master/angular-material.js",
+      "github:angular/bower-angular-aria@1.5.8.js",
+      "github:angular/bower-angular-aria@1.5.8/angular-aria.js",
+      "github:angular/bower-angular-animate@1.5.8.js",
+      "github:angular/bower-angular-animate@1.5.8/angular-animate.js"
+    ]
   },
 
   map: {
